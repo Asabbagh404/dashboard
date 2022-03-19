ed2a853c995f0b76e861d595dd27c72072c5b5b0
Sabbagh <anthonysabbagh03@gmail.com>
2022-03-19 17:45:51 +0100

    add router links

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,81 @@
 <template>
   <v-app>
     <div
-      class="filler"
+        class="filler"
     ></div>
     <v-system-bar
-        height="30"
-        window
-        color="purple darken-3"
+        color="purple darken-4"
         fixed
-        class="d-flex justify-space-around"
+        class="d-flex justify-space-around text-caption"
     >
-      <span class="white--text">Domains</span>
-      <span class="white--text text-uppercase">https://cisotrust.fr</span>
-      <span class="white--text">Signed in as Franck</span>
+      <span class="white--text text-caption">Domains</span>
+      <span class="white--text text-uppercase text-caption">https://cisotrust.fr</span>
+      <span class="white--text text-caption">Signed in as Franck</span>
     </v-system-bar>
     <v-navigation-drawer
-            permanent
-            left
-            app
-            fixed
-            expand-on-hover
-        >
-          <v-list>
-            <v-list-item class="px-2">
-              <v-list-item-avatar>
-                <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
-              </v-list-item-avatar>
-            </v-list-item>
-          </v-list>
+        permanent
+        left
+        app
+        fixed
+        expand-on-hover
+    >
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+      </v-list>
 
-          <v-divider></v-divider>
+      <v-divider></v-divider>
 
-          <v-list
-              nav
-              dense
-          >
-            <v-list-item link>
-              <v-list-item-icon>
-                <v-icon>mdi-folder</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>My Files</v-list-item-title>
-            </v-list-item>
-            <v-list-item link>
-              <v-list-item-icon>
-                <v-icon>mdi-account-multiple</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Shared with me</v-list-item-title>
-            </v-list-item>
-            <v-list-item link>
-              <v-list-item-icon>
-                <v-icon>mdi-star</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Starred</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-navigation-drawer>
+      <v-list
+          nav
+          dense
+      >
+        <router-link to="/">
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon color="purple darken-4">mdi-layers</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Website</v-list-item-title>
+          </v-list-item>
+        </router-link>
+        <hr>
+        <router-link to="/">
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon color="black">mdi-briefcase</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Inventory</v-list-item-title>
+          </v-list-item>
+        </router-link>
+        <router-link to="/test">
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon color="black">mdi-shield-account-variant</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Domains</v-list-item-title>
+          </v-list-item>
+        </router-link>
+        <router-link to="/">
+          <v-list-item link disabled>
+            <v-list-item-icon>
+              <v-icon color="black" disabled>mdi-earth</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Privacy</v-list-item-title>
+          </v-list-item>
+        </router-link>
+        <router-link to="/">
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon color="black">mdi-bell</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Alerts</v-list-item-title>
+          </v-list-item>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
       <router-view/>
     </v-main>
@@ -76,7 +97,11 @@ export default {
   width: 50px;
   height: 36px;
 }
+
 .v-system-bar {
   left: 50px;
+}
+a {
+  color: transparent !important;
 }
 </style>
